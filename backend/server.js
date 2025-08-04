b69bf006ef8ac4969985dc305f67f7eceee3c3c2
Mazen mohamed <mazendaghash@gmail.com>
2025-08-04 14:39:30 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: 'localhost',       
   user: 'root',           
-  password: 'Daghash1029',           
+  password: '',           
   database: 'auth_db'     
 });
 
